152705dd86fcea0c924325777de1f7e0f95de277
Daniel Flavio <danflavio@gmail.com>
2026-03-15 18:09:05 -0300

    Ajuste arquivo yml

--- init.sql
+++ init.sql
@@ -5,7 +5,3 @@ CREATE TABLE IF NOT EXISTS posts (
     author VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Insere um registro inicial para garantir que o teste de listagem não venha vazio
-INSERT INTO posts (title, content, author) 
-VALUES ('Aula Inicial', 'Conteúdo da aula de teste.', 'Professor Dan');
